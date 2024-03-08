66ec3c0c6ecdf081b0f1594e32d59a80d207f995
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-03-08 14:46:22 +0000

    Use google API instead of faviconkit due to ssl certificates not being renewed

--- src/Data/favicon.ts
+++ src/Data/favicon.ts
@@ -1,5 +1,5 @@
 export const faviconFromUrl = (url: string): string => {
   let domainUrl: URL = new URL(url);
   const domain = domainUrl.hostname.replace('www.', '');
-  return `https://puny-yellow-llama.faviconkit.com/${domain}/256`;
+  return `https://www.google.com/s2/favicons?domain=${domain}&sz=256`;
 };
