157db3503072d57516e6e209195c6748be1d96f4
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-03-08 16:21:27 +0000

    Update tests

--- tests/unit/Data/favicon.test.tsx
+++ tests/unit/Data/favicon.test.tsx
@@ -3,14 +3,16 @@ import { faviconFromUrl } from '../../../src/Data/favicon';
 describe('faviconFromUrl', () => {
   it('returns a valid URL when given a valid input URL', () => {
     const url = 'https://www.google.com';
-    const expected = 'https://puny-yellow-llama.faviconkit.com/google.com/256';
+    const expected =
+      'https://www.google.com/s2/favicons?domain=google.com&sz=256';
     const actual = faviconFromUrl(url);
     expect(actual).toBe(expected);
   });
 
   it('removes the "www." subdomain from the input URL hostname', () => {
     const url = 'https://www.example.com';
-    const expected = 'https://puny-yellow-llama.faviconkit.com/example.com/256';
+    const expected =
+      'https://www.google.com/s2/favicons?domain=example.com&sz=256';
     const actual = faviconFromUrl(url);
     expect(actual).toBe(expected);
   });
