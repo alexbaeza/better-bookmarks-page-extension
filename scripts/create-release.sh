ae70103c558f7fa143506a39654590684fbcd298
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-03-10 22:06:09 +0000

    Fix github action

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -12,30 +12,31 @@ echo "Updating manifests with correct version"
 VERSION_STRING='"version": '
 CURR_VERSION=$(./scripts/current-version-check.sh)
 
+cd build || exit
+
 echo "Creating Firefox release"
-mv build/manifest-firefox.json build/manifest.json
+mv manifest-firefox.json manifest.json
 
 echo "Setting Firefox version"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" build/manifest.json
+  sed -i '' -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" manifest.json
 else
-  sed -i -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" build/manifest.json
+  sed -i -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" manifest.json
 fi
 
 echo "Bundling Firefox release"
-
-zip -r ./build/release/extension-firefox-$CURR_VERSION.zip ./* -x 'manifest-*' -x 'build/release/*'
+zip -r ./release/extension-firefox-$CURR_VERSION.zip ./* -x 'manifest-*' -x 'release/*'
 
 echo "Creating Chrome release"
-mv build/manifest-chrome.json build/manifest.json
+mv manifest-chrome.json manifest.json
 echo "Setting Chrome version"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" build/manifest.json
+  sed -i '' -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" manifest.json
 else
-  sed -i -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" build/manifest.json
+  sed -i -e "s/\($VERSION_STRING\).*/\1\"$CURR_VERSION\",/" manifest.json
 fi
 
 echo "Bundling Chrome release"
-zip -r ./build/release/extension-chrome-$CURR_VERSION.zip ./* -x 'manifest-*' -x 'build/release/*'
+zip -r ./release/extension-chrome-$CURR_VERSION.zip ./* -x 'manifest-*' -x 'release/*'
 
 echo "Completed successfully"
