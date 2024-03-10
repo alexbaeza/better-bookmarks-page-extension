b1241c53e677bea15515f671eacb6a1ec48fbe1a
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-03-10 21:29:13 +0000

    Debug github action

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -7,4 +7,4 @@ NEXT_VERSION=$(./scripts/release-version-check.sh $UPDATE_TYPE)
 
 echo "Updating version from $CURR_VERSION -> $NEXT_VERSION"
 VERSION_STRING='"version": '
-sed -i '' "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" package.json
+sed -i '' "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" ./package.json
