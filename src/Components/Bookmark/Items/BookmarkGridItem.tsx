4002530ae629be12c7cde96b113ac565f4e70286
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-09-22 16:15:37 +0100

    fix: fix incorrect backgrounds Add eslint plugin for tailscale to enforce consistency '

--- src/Components/Bookmark/Items/BookmarkGridItem.tsx
+++ src/Components/Bookmark/Items/BookmarkGridItem.tsx
@@ -4,7 +4,6 @@ import { faviconFromUrl } from '../../../Data/favicon';
 
 export type BookmarkGridItemProps = {
   dataTestId?: string;
-
   title: string;
   url: string;
 };
@@ -15,19 +14,19 @@ export const BookmarkGridItem = ({
   url
 }: BookmarkGridItemProps) => {
   return (
-    <div className="flex w-20 flex-col items-center justify-center break-all rounded-lg bg-primary-dark-contrast bg-opacity-20 p-2 transition duration-300 ease-out hover:scale-105 ">
+    <div className="group/item flex w-20 flex-col items-center justify-center break-all rounded-lg bg-secondary-dark p-2 transition duration-300 ease-out hover:scale-105 hover:bg-secondary-dark-active ">
       <a data-testid={dataTestId} href={url}>
-        <div className="mx-auto flex h-16 w-16 transform cursor-pointer items-center justify-center rounded-lg bg-secondary-dark  hover:bg-secondary-dark-active">
+        <div className="mx-auto flex size-16 cursor-pointer items-center justify-center rounded-lg bg-primary-dark-contrast bg-opacity-10 group-hover/item:bg-primary-dark-active">
           <ImageWithFallback
             data-testid="image-with-fallback"
-            className="h-12 w-12 overflow-hidden rounded-lg"
+            className="size-12 overflow-hidden rounded-lg"
             src={faviconFromUrl(url)}
             alt={`Bookmark item for ${title}`}
           />
         </div>
       </a>
       <div className="mt-1 flex cursor-pointer items-center justify-center">
-        <p className="line-clamp-2 h-8 min-h-full text-xs text-text-primary break-words">
+        <p className="line-clamp-2 h-8 min-h-full break-words text-xs text-text-primary">
           {title}
         </p>
       </div>
