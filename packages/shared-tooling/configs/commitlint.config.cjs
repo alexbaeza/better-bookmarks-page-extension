689fad558018c4b3e17f5eb9ac680cf8bba263ba
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2025-10-25 22:22:18 +0100

    chore: more fixes and improvements

--- packages/shared-tooling/configs/commitlint.config.cjs
+++ packages/shared-tooling/configs/commitlint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
+    'type-enum': [2, 'always', ['release', 'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'subject-case': [0], // Disabled - don't enforce casing for commit subjects
