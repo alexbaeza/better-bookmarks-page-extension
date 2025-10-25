5b825931c5384bf5a7017c678b79e4508e675990
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2025-10-25 20:19:22 +0100

    chore: disable casing

--- packages/shared-tooling/configs/commitlint.config.cjs
+++ packages/shared-tooling/configs/commitlint.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0], // Disabled - don't enforce casing for commit subjects
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
   },
