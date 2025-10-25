b8e0177a07dafaeb43d76ce9021e11dc3a818d98
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2025-10-25 23:12:32 +0100

    chore: simplify

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,6 @@
-packages/shared-tooling/configs/commitlint.config.cjs
+const commitlintSharedConfig = require('@better-bookmarks/shared-tooling').commitlint;
+
+module.exports = {
+  ...commitlintSharedConfig,
+  // Project-specific overrides can be added here if needed
+};
